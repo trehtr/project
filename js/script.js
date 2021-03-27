e8830837a1460ae6c2272cebd632136f14276fde
Gennadiy <example@gmail.com>
2021-03-28 02:38:23 +0300

    first lesson

--- js/script.js
+++ js/script.js
@@ -2,3 +2,19 @@ let number = 5;
 const leftBorderWidth = 1;
 number = 10;
 console.log(number);
+
+const numberOfFilms = +prompt('Сколько фильмов вы посмотрели?', '');
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const a = prompt('asfa', ''),
+      b = prompt('dskg', ''),
+      c = prompt('f', '')
+
+personalMovieDB.movies[a] = b;
